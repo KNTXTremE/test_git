9a25771b41b2ce1308dd8699f2544adb47bf828a
KNTXTremE <Kan.Horwang@gmail.com>
2017-08-25 12:01:17 +0700

    add src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-package PACKAGE_NAME;
-
 /**
  * Created by KanTLovE on 8/25/2017.
  */
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Type width");
+        int width = Integer.parseInt(keyboard.nextLine());
+        System.out.print("Type height");
+        int height = Integer.parseInt(keyboard.nextLine());
+        System.out.print(width*height);
+    }
 }
